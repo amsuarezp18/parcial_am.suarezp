b3f722b9d6d7339e7d9b3e8bdd97f69e7183ac45
amsuarezp <amsuarezp@gmail.com>
2020-09-22 20:57:06 -0500

    inline items

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ fetch("https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173da
     }));
 
 function addProduct(product, section) {
-    section.innerHTML += `<div class="card col-3 my-3" style="max-width: 18rem; min-width: 18rem;">
+    section.innerHTML += `<div class="card col-3 my-3" style="max-width: 15.2rem; min-width: 15.2rem;">
     <img class="card-img-top" src="${product.image}" alt="${product.name}">
     <div class="card-body">
       <h5 class="card-title">${product.name}</h5>
@@ -29,7 +29,7 @@ function addProduct(product, section) {
 
 function addToCar(param){
     cart.push(param)
-    document.getElementById("item-cart").innerHTML = "Items";
+    document.getElementById("item-cart").innerHTML = "Items " + cart.length;
     
     var tabla = document.getElementsByClassName("body-table")[0];
     var row = tabla.insertRow(-1);
